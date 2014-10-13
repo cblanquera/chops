3773e9eb5adec4a9c75ac5996de5f0d7d3e37437
Christian Blanquera <cblanquera@openovate.com>
2014-10-13 17:09:06 +0800

    changed state data format.

--- chops.js
+++ chops.js
@@ -172,62 +172,172 @@
 		
 		/* Private Methods
 		-------------------------------*/
-		var __expand = function(data) {
-			var i, dot, path, pointer, object = {};
+		var __queryToHash = function(data) {
+			var hash = {};
 			
-			for(var key in data) {
-				if(data.hasOwnProperty(key)) {
-					if(key.indexOf('[') === -1) {
-						object[key] = data[key];
+			//if empty data
+			if(data.length == 0) {
+				//return empty hash
+				return {};
+			}
+			
+			//split the query by &
+			var queryArray = data.split('&');
+			
+			//loop through the query array
+			for (var propertyArray, hashNameArray, 
+			curent, next, name, value, j, i = 0; 
+			i < queryArray.length; i++) {
+				//split name and value
+				propertyArray = queryArray[i].split('=');
+				
+				propertyArray[1] = propertyArray[1] || '';
+				
+				//url decode both name and value
+				name = decodeURIComponent(propertyArray[0].replace(/\+/g, '%20'));
+				value = decodeURIComponent(propertyArray[1].replace(/\+/g, '%20'));
+				
+				//if no value
+				if (!propertyArray[1]) {
+					//if no name
+					if(!propertyArray[0]) {
+						//do nothing
 						continue;
 					}
 					
-					dot = key
-						.replace(/\]\[/g, '.')
-						.replace(/\[/g, '.')
-						.replace(/\]/g, '')
+					value = null;
+				}
+				
+				//At this point, we have a key and value
+				
+				//is value a a string but a number ?
+				//and there is a decimal ?
+				if(typeof value == 'string' 
+				&& !/[a-zA-Z\+]/.test(value)
+				&& !/^0/.test(value)
+				&& !isNaN(parseFloat(value))
+				&& value.indexOf('.') != -1) {
+					value = parseFloat(value);
+				//is value a a string but a number ?
+				} else if(typeof value == 'string' 
+				&& !/[a-zA-Z\+]/.test(value)
+				&& !/^0/.test(value) 
+				&& !isNaN(parseFloat(value))) {
+					value = parseInt(value);
+				}
+				
+				//if no array marker
+				if(name.indexOf('[') == -1) {
+					//simply put it in hash
+					hash[name] = value;
+					//we are done
+					continue;
+				}
+				
+				//At this point, we have a hash key and value
+				
+				//BEFORE:
+				//hash[key1][some][]
+				//hash[][some][key1]
+				
+				hashNameArray = name.split('[');
+				
+				//AFTER:
+				//hash, key1], some, ]
+				//hash, ], some], key1]
+				
+				current = hash;
+				for(j = 0; j < hashNameArray.length; j++) {
+					//remove straggling ]
+					name = hashNameArray[j].replace(']', '');
 					
-					//it hash a .
-					//key1.0.key2
-					path = dot.split('.');
-					pointer = object;
+					//is there more names ?
+					if((j + 1) == hashNameArray.length) {
+						//we are done
+						break;
+					}
 					
-					for(var i = 0; i < path.length; i++) {
-						if(typeof pointer[path[i]] === 'undefined') {
-							//if there is a next path
-							//and it is a number
-							if(typeof path[i+1] !== 'undefined'
-							&& !isNaN(parseInt(path[i+1]))) {
-								pointer[path[i]] = [];
-							} else {
-								pointer[path[i]] = {};
-							}
+					//at this point there are more names
+					//hash, key1, some, ]
+					//hash, ], some], key1]
+					
+					//does it exist ? 
+					if(!current[name]) {
+						next =  {}
+						
+						//if no name
+						//it is possible for numbers to be the name
+						if(hashNameArray[j + 1] == ']'
+						|| (!isNaN(parseFloat(hashNameArray[j + 1].replace(']', ''))) 
+						&& isFinite(hashNameArray[j + 1].replace(']', '')))) {
+							next = [];
 						}
 						
-						if(i < (path.length - 1)) {
-							//move on
-							pointer = pointer[path[i]];
+						
+						//is the current an array ?
+						if(current instanceof Array) {
+							current.push(next);
+						} else {
+							current[name] = next;
 						}
 					}
 					
-					pointer[path[i - 1]] = data[key];
+					//at this point next exists
+					
+					//is the current an array ?
+					if(current instanceof Array) {
+						//traverse
+						current = current[current.length - 1];
+						continue;
+					}
+					
+					//traverse
+					current = current[name];
+				}
+				
+				//is the current an array ?
+				if(current instanceof Array) {
+					current.push(value);
+					continue;
+				}
+				
+				//current can be undefined because it reached
+				//a datatype that cannot be traversable
+				if(current) {
+					current[name] = value;
 				}
 			}
 			
-			return object;
+			return hash;
+		};
+		
+		var __replace = function(data, files) {
+			for(var key in data) {
+				if(data.hasOwnProperty(key)) {
+					if(typeof data[key] === 'string' && data[key].indexOf('____CHOPS____') === 0) {
+						if(typeof files[data[key].substr(13)] === 'undefined') {
+							delete data[key];
+						}
+						
+						data[key] = files[data[key].substr(13)];
+					}
+					
+					if(typeof data[key] === 'object' && data[key] !== null) {
+						__replace(data[key], files);
+					}
+				}
+			}
 		};
 		
 		var __getState = function(url) {
 			var state = { 
 				url		: url,
-				query	: '', 
+				query	: '', //query string
+				json	: '', //json string; no files
 				method	: 'GET', 
-				data	: {
-					flattened	: {},
-					expanded	: {},
-					serialized	: {},
-					json		: {}
-				} };
+				data	: {}, //combined data in common js object
+				serial	: [], //serialized array; no files
+				files	: [] }; //serialized array; just files
 			
 			//if there is a ?
 			if(state.url.indexOf('?') !== -1) {
@@ -239,7 +349,10 @@
 				state.url = state.url.substr(window.location.origin.length);
 			}
 			
-			var flat = {}, query = [];
+			state.data = __queryToHash(state.query);
+			state.json = JSON.stringify(state.data);
+			
+			var query = [];
 			
 			if(state.query.length) {
 				query = state.query.split('&');
@@ -247,11 +360,8 @@
 			
 			for(var setting, i = 0; i < query.length; i++) {
 				setting = query[i].split('=');
-				flat[setting.shift()] = setting.join('=');
+				state.serial.push({name: setting.shift(), value: settings.join('=')});
 			}
-		
-			state.data.serialized = state.data.flattened = flat;
-			state.data.json = state.data.expanded = __expand(state.data.serialized);
 			
 			return state;
 		};
@@ -268,30 +378,16 @@
 			
 			var state = __getState(url);
 			
-			state.method = $(form).attr('method') || 'GET'; 
-			
-			//dont use the data from the URL
-			state.data.flattened 	= {};
-			state.data.serialized 	= {};
-			
 			//populate state with what we know
-			state.method = state.method.toUpperCase();
-			
-			var flat = $(form).serializeArray();
-			
-			for(var i = 0; i < flat.length; i++) {
-				state.data.flattened[flat[i].name] 	= flat[i].value;
-				state.data.serialized[flat[i].name] = flat[i].value;
-			}
-			
-			//make an expanded version of the data
-			state.data.json = __expand(state.data.serialized);
+			state.method 	= $(form).attr('method') || 'GET'; 
+			state.method 	= state.method.toUpperCase();
+			state.query 	= $(form).serialize();
+			state.serial 	= $(form).serializeArray();
+			state.data		= __queryToHash(state.query);
+			state.json 		= JSON.stringify(state.data);
 			
 			//is it a GET request ?
 			if(state.method === 'GET') {
-				//remove the current query with a form generated one
-				state.query = $(form).serialize();
-				
 				//manually form the HREF
 				//if there is a ?
 				if(state.url.indexOf('?') !== -1) {
@@ -302,7 +398,7 @@
 				state.url += '?' + state.query;
 			}
 			
-			//it is a post, delete, put, whateva ...
+			var files = { query: state.query, items: {} };
 			
 			//is there files?
 			$('input[type="file"]', form).each(function() {
@@ -313,13 +409,20 @@
 				}
 				
 				//store the files
-				for(var i = 0; i < this.files.length; i++) {
-					state.data.flattened[this.name + '[' + i + ']'] = this.files[i];
+				for(var j = 0, i = 0; i < this.files.length; i++) {
+					state.files.push({ name: this.name + '[]', value: this.files[i] });
+					files.items[++j] = this.files[i];
+					
+					//add this to state.data
+					files.query += '&' + this.name + '[]=____CHOPS____' + j;
 				}
 			});
 			
-			//make an expanded version of the data
-			state.data.expanded = __expand(state.data.flattened);
+			files.data = __queryToHash(files.query);
+			
+			__replace(files.data, files.items);
+			
+			state.data = files.data;
 			
 			//push the state
 			window.history.pushState(state, '', state.url);
